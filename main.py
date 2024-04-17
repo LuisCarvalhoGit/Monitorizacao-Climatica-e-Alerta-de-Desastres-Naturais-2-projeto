046cdf6bf552415667122fc2165d9c0d1756a640
Luis Carvalho <pro.luis.carvalho@gmail.com>
2024-04-17 23:36:47 +0100

    main

--- main.py
+++ main.py
@@ -3,11 +3,30 @@ import customtkinter
 import tkinter
 import datetime as dt
 
-def get_weather_data(api_key, location):
-    base_url = "https://api.openweathermap.org/data/2.5/weather?"
-    params = {"key": api_key, "q": location}
+def kelvin_to_celsius_fahrenheit(kelvin):
+    celsius = kelvin - 273.15
+    fahrenheit = celsius * (9/5) + 32
+    return celsius,fahrenheit
 
-    response = requests.get(base_url, params=params)
-    data = response.json()
-    return data
+def get_weather_data(api_key, lat, lon):
+    base_url = "https://api.openweathermap.org/data/2.5/weather?"
+    url= base_url + "appid=" + api_key + "&lat=" + lat + "&lon=" + lon
+
+    response = requests.get(url).json()
+
+    temp_kelvin = response ["main"]["temp"]
+    temp_celsius, temp_fahrenheit = kelvin_to_celsius_fahrenheit(temp_kelvin)
+    print(temp_kelvin)
+    print(temp_celsius)
+
+
+get_weather_data("8ba62249b68f6b02f4cc69cae7495cb3", "41.295900", "-7.746350")  # usar com cuidado, existe limite
+
+
+#   "https://api.openweathermap.org/data/2.5/weather?lat=41.295900&lon=-7.746350&appid=8ba62249b68f6b02f4cc69cae7495cb3"  para ver em JSON
+
+# Latitule Vila Real,PT -> 41.295900 , Longitude Vila Real,PT -> -7.746350
+
+
+
 
