e7ea3798ed74e25996dcc7273ae7bd2b3ccb2a6e
Luis Carvalho <pro.luis.carvalho@gmail.com>
2024-04-18 15:16:27 +0100

    main

--- main.py
+++ main.py
@@ -9,9 +9,9 @@ def kelvin_to_celsius_fahrenheit(kelvin):
     fahrenheit = celsius * (9/5) + 32
     return celsius,fahrenheit
 
-def get_weather_data(api_key, lat, lon):
+def get_weather_data(api_key, cidade):
     base_url = "https://api.openweathermap.org/data/2.5/weather?"
-    url= base_url + "appid=" + api_key + "&lat=" + lat + "&lon=" + lon
+    url= base_url + "appid=" + api_key + "&q=" + cidade
 
     response = requests.get(url).json()
 
@@ -19,13 +19,17 @@ def get_weather_data(api_key, lat, lon):
         print("Cidade nao encontrada")
     else:
         temp_kelvin = response["main"]["temp"]
+        wind_speed = response['wind']['speed']
         temp_celsius, temp_fahrenheit = kelvin_to_celsius_fahrenheit(temp_kelvin)
-        print(temp_kelvin)
-        print(temp_celsius)
+        humidade = response['main']['humidity']
+        print(f"Temperatura em {cidade}(K): {temp_kelvin} K")
+        print(f"Temperatura em {cidade}(ºC): {temp_celsius:.2f} ºC")
+        print(f"Velocidade do Vento em {cidade}: {wind_speed} m/s")
+        print(f"Humidade em {cidade}: {humidade}")
 
 
 
-get_weather_data("8ba62249b68f6b02f4cc69cae7495cb3", "41.295900", "-7.746350")  # usar com cuidado, existe limite
+get_weather_data("8ba62249b68f6b02f4cc69cae7495cb3", "Vila Real,PT")  # usar com cuidado, existe limite
 
 
 #   "https://api.openweathermap.org/data/2.5/weather?lat=41.295900&lon=-7.746350&appid=8ba62249b68f6b02f4cc69cae7495cb3" para ver Vila Real, PT em JSON
