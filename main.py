dd90ee917e3ca6811e8b5d79b3eaf83999efb9b7
Luis Carvalho <pro.luis.carvalho@gmail.com>
2024-05-13 15:42:31 +0100

    main

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ import requests
 import customtkinter as ctk
 import tkinter as tk
 import pandas as pd
-import matplotlib as mp
+import matplotlib.pyplot as plt
+from dateutil import parser
 import smtplib
+from email.mime.text import MIMEText
 import time
 import sqlite3
 from tabulate import tabulate
@@ -155,9 +157,34 @@ def checkDisasters(weather_data_df):
     if weather_data_df['wind_speed'].mean() > limite_vento_furacao:
         print("Alerta! Condições potenciais de furacão detectadas.")
 
+def plot_data():
+    data = get_data_from_db()
+
+    temp = [row[1] for row in data]
+    temp_feels_like = [row[2] for row in data]
+    wind_speed = [row[3] for row in data]
+    humidade = [row[4] for row in data]
+    quant_nuvens = [row[5] for row in data]
+
+    plt.figure(figsize=(10, 6))
+    plt.plot(temp, label='Temperatura (ºC)')
+    plt.plot(temp_feels_like, label='Sensação Térmica (ºC)')
+    plt.plot(wind_speed, label='Velocidade do Vento (m/s)')
+    plt.plot(humidade, label='Humidade')
+    plt.plot(quant_nuvens, label='Quantidade de Nuvens')
+    plt.title('Weather Data over Time')
+    plt.xlabel('Tempo')
+    plt.ylabel('Valores')
+    plt.legend()
+    plt.show()
 
 
-print_db()
+#print_db()
+
+#plot_data()
+
+
+
 
 
 
@@ -230,7 +257,7 @@ def criar_interface():
         analyze_weather_data(weather_data_df)
 
     # Adicionando um botão
-    botao = ctk.CTkButton(janela, text='Obter dados meteorológicos', command=mostrar_nome)
+    botao = ctk.CTkButton(janela, text='Obter dados meteorológicos', command=plot_data())
     botao.pack()
 
     # Iniciando o loop principal da GUI
